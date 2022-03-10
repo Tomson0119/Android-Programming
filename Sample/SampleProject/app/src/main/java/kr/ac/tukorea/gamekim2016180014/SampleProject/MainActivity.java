df9042b6655143c0d0e2ceabc07596f655958f3c
Tomson0119 <kjs980119@gmail.com>
2022-03-10 17:20:37 +0900

    버튼, 텍스트 뷰와 상호작용 테스트

--- Sample/SampleProject/app/src/main/java/kr/ac/tukorea/gamekim2016180014/SampleProject/MainActivity.java
+++ Sample/SampleProject/app/src/main/java/kr/ac/tukorea/gamekim2016180014/SampleProject/MainActivity.java
@@ -1,14 +1,32 @@
 package kr.ac.tukorea.gamekim2016180014.SampleProject;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
+/**
+ * <p>extends : 클래스 상속.</p>
+ * <p>MainActivity : 프로그램 시작점.</p>
+ * <p>manifests : application의 기본 설정들을 모아놓은 파일.</p>
+ * <p>activity들을 여러 개 넣을 수 있으며, icon, theme 등을 설정할 수 있다.</p>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtView = findViewById(R.id.nameText);
+        txtView.setText("Kim Ji Sung");
+    }
+
+    private void onButtonPushed() {
+        TextView txtView = findViewById(R.id.nameText);
+        txtView.setText("Button has pushed");
     }
 }
