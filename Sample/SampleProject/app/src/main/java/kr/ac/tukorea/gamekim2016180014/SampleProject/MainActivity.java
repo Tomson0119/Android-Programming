d0b7eed166d3a14806318b02ec14f474efe7b07f
Tomson0119 <kjs980119@gmail.com>
2022-03-16 11:12:52 +0900

    버튼 기능 추가 및 이미지 변경

--- Sample/SampleProject/app/src/main/java/kr/ac/tukorea/gamekim2016180014/SampleProject/MainActivity.java
+++ Sample/SampleProject/app/src/main/java/kr/ac/tukorea/gamekim2016180014/SampleProject/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,19 +20,49 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final int MAX_PAGE = 5;
+    protected static int page = 0;
+
+    private static final int[] ImageIds = new int[] {
+            R.mipmap.cat1,
+            R.mipmap.cat2,
+            R.mipmap.cat3,
+            R.mipmap.cat4,
+            R.mipmap.cat5
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setPage(1);
     }
 
     public void onButtonPrev(View view) {
-        R.mipmap.
+        setPage(page-1);
         Log.d(TAG, "Button prev pushed");
     }
 
     public void onButtonNext(View view) {
+        setPage(page+1);
         Log.d(TAG, "Button next pushed.");
+    }
+
+    private void setPage(int newPage) {
+        if(newPage <= 0) {
+            newPage = MAX_PAGE;
+        }
+        else if(newPage > MAX_PAGE) {
+            newPage = 1;
+        }
+        page = newPage;
+
+        String text = page + " / " + MAX_PAGE;
+        TextView view = findViewById(R.id.pageText);
+        view.setText(text);
+
+
+        ImageView img = findViewById(R.id.contentImageView);
+        img.setImageResource(ImageIds[page-1]);
     }
 }
