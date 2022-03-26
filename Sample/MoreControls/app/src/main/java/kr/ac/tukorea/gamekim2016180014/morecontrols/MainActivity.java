03ef6b857f9364fa18aca049e7151a55f819a5a8
Tomson0119 <kjs980119@gmail.com>
2022-03-26 10:38:04 +0900

    textWatcher를 전역에서 미리 생성하여 적용.

--- Sample/MoreControls/app/src/main/java/kr/ac/tukorea/gamekim2016180014/morecontrols/MainActivity.java
+++ Sample/MoreControls/app/src/main/java/kr/ac/tukorea/gamekim2016180014/morecontrols/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private EditText editText;
     private TextView outputText;
+    private TextWatcher editWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            outputText.setText(charSequence.toString());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.nameEdit);
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                
-            }
-        });
+        editText.addTextChangedListener(editWatcher);
         outputText = findViewById(R.id.outputText);
     }
 
