520f98c498985ec1866a92bcaa97f52b69072afd
Tomson0119 <kjs980119@gmail.com>
2022-04-07 21:55:04 +0900

    image의 사각형 경계선을 표현, 사각형 이동 및, 반환 함수 추가

--- TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Image.java
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Image.java
@@ -2,18 +2,42 @@ package kr.ac.tukorea.gamekim2016180014.touchandslice;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 
 public class Image {
+    protected Rect border;
+    protected Paint borderPaint;
     protected Bitmap bitmap;
     protected RectF dstRect;
 
     public Image(int bitmapId) {
         bitmap = BitmapPool.getBitmap(bitmapId);
-        dstRect = new RectF(0,0, 100, 100);
+        createBorder();
+        float width = Metrics.getSize(R.dimen.image_width);
+        float height = Metrics.getSize(R.dimen.image_height);
+        dstRect = new RectF(-width / 2,-height/2, width/2, height/2);
+    }
+
+    private void createBorder() {
+        borderPaint = new Paint();
+        borderPaint.setColor(Color.BLACK);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(2);
     }
 
     public void draw(Canvas canvas) {
         canvas.drawBitmap(bitmap, null, dstRect, null);
+        canvas.drawRect(dstRect, borderPaint);
+    }
+
+    public void move(float x, float y) {
+        dstRect.offset(x, y);
+    }
+
+    public RectF getRect() {
+        return dstRect;
     }
 }
