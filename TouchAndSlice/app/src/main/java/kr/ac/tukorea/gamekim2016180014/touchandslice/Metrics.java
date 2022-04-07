a17e05205d1a8d29afb79daa56f3f8157b2680ba
Tomson0119 <kjs980119@gmail.com>
2022-04-07 21:53:53 +0900

    Metrics, resource의 dp의 사이즈, float 값 반환 함수 추가

--- TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Metrics.java
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Metrics.java
@@ -1,5 +1,8 @@
 package kr.ac.tukorea.gamekim2016180014.touchandslice;
 
+import android.content.res.Resources;
+import android.util.TypedValue;
+
 public class Metrics {
     public static int width;
     public static int height;
@@ -7,5 +10,17 @@ public class Metrics {
     public static void setResolution(int w, int h) {
         width = w;
         height = h;
+    }
+
+    public static float getSize(int dimenResId) {
+        Resources res = ScreenView.view.getResources();
+        return res.getDimension(dimenResId);
+    }
+
+    public static float floatValue(int dimenResId) {
+        Resources res = ScreenView.view.getResources();
+        TypedValue outValue = new TypedValue();
+        res.getValue(dimenResId, outValue, true);
+        return outValue.getFloat();
     }
 }
