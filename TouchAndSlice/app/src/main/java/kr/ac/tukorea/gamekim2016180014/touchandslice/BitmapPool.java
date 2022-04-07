ce30849c23526b4cf4181e61eb1935def4eceff0
Tomson0119 <kjs980119@gmail.com>
2022-04-07 20:58:54 +0900

    Bitmap pool, Metrics 클래스 추가

---
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/BitmapPool.java
@@ -0,0 +1,21 @@
+package kr.ac.tukorea.gamekim2016180014.touchandslice;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.HashMap;
+
+public class BitmapPool {
+    private static HashMap<Integer, Bitmap> pool = new HashMap<>();
+
+    public static Bitmap getBitmap(int bitmapId) {
+        Bitmap bitmap = pool.get(bitmapId);
+        if(bitmap == null) {
+            Resources res = ScreenView.view.getResources();
+            bitmap = BitmapFactory.decodeResource(res, bitmapId);
+            pool.put(bitmapId, bitmap);
+        }
+        return bitmap;
+    }
+}
---
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Metrics.java
@@ -0,0 +1,11 @@
+package kr.ac.tukorea.gamekim2016180014.touchandslice;
+
+public class Metrics {
+    public static int width;
+    public static int height;
+
+    public static void setResolution(int w, int h) {
+        width = w;
+        height = h;
+    }
+}
