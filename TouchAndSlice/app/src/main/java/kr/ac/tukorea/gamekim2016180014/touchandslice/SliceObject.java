625ffa70411fece10e0f77a4de1a56e9e4c0fe4e
Tomson0119 <kjs980119@gmail.com>
2022-04-14 15:16:41 +0900

    slice point 2개를 준비하여 slope에 따라 위치를 결정한다. 두 포인트로 구성한 라인은 center 포인트를 꼭 포함해야 한다.

--- TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/SliceObject.java
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/SliceObject.java
@@ -8,20 +8,20 @@ import android.graphics.RectF;
 
 public class SliceObject implements GameObject {
     private final Image image;
-    private float x;
-    private float y;
-
-    private float rotSpeed;
+    private PointF center;
+    private PointF[] slicePoints;
+    private Paint slicePaint;
 
     public SliceObject(float x, float y, int imageId) {
         image = new Image(imageId);
-        this.x = x;
-        this.y = y;
+        center = new PointF(x, y);
         image.move(x, y);
-    }
-
-    public void setRotationSpeed(float speed) {
-        image.setRotation(speed);
+        slicePoints = new PointF[2];
+        slicePoints[0] = new PointF();
+        slicePoints[1] = new PointF();
+        slicePaint = new Paint();
+        slicePaint.setColor(Color.BLUE);
+        slicePaint.setStrokeWidth(10);
     }
 
     @Override
@@ -32,9 +32,38 @@ public class SliceObject implements GameObject {
     @Override
     public void draw(Canvas canvas) {
         image.draw(canvas);
+        for(PointF p : slicePoints) {
+            canvas.drawPoint(p.x, p.y, slicePaint);
+        }
     }
 
     public RectF getRect() {
         return image.getRect();
+    }
+
+    public void slice(float slope) {
+        setSlicePoints(slope);
+
+        PointF[] vertices = image.getVertices();
+        for(int i=0;i<vertices.length;i++) {
+            PointF p11 = vertices[i];
+            PointF p12 = vertices[(i+1)%vertices.length];
+        }
+    }
+
+    private void setSlicePoints(float slope) {
+        RectF rect = image.getRect();
+        if(slope < Float.MAX_VALUE) {
+            if(Math.abs(slope) <= 1.0f) {
+                slicePoints[0].set(center.x - 200.0f, center.y - 200.0f * slope);
+                slicePoints[1].set(center.x + 200.0f, center.y + 200.0f * slope);
+            } else {
+                slicePoints[0].set(center.x - 200.0f / slope, center.y - 200.0f);
+                slicePoints[1].set(center.x + 200.0f / slope, center.y + 200.0f);
+            }
+        } else {
+            slicePoints[0].set(center.x, center.y - 200.0f);
+            slicePoints[1].set(center.x, center.y + 200.0f);
+        }
     }
 }
