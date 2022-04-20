7b909fdc9a52b4269239bc827ee61dd4f647f780
Tomson0119 <kjs980119@gmail.com>
2022-04-20 22:41:15 +0900

    디버깅용 rectPaint 생성하여 rect 경계 표시,

--- TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Image.java
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Image.java
@@ -4,13 +4,15 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 
 public class Image extends CustomRect {
     protected Bitmap bitmap;
-    private Paint bitmapPaint;
+
+    private Paint rectPaint;
+    private Rect srcRect;
 
     public Image(int bitmapId) {
         super();
@@ -24,8 +26,10 @@ public class Image extends CustomRect {
 
     private void init(int bitmapId, float width, float height) {
         bitmap = BitmapPool.getBitmap(bitmapId);
-        bitmapPaint = new Paint();
-        bitmapPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OVER));
+        rectPaint = new Paint();
+        rectPaint.setColor(Color.BLACK);
+        rectPaint.setStyle(Paint.Style.STROKE);
+        rectPaint.setStrokeWidth(5);
         initAsRect(width, height);
     }
 
@@ -35,6 +39,7 @@ public class Image extends CustomRect {
     }
 
     public void drawRect(Canvas canvas) {
+        canvas.drawRect(getRect(), rectPaint);
         canvas.drawBitmap(bitmap, null, getRect(), null);
     }
 }
