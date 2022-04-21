204f4c238c91d46968d0017169bc6a337f66f944
Tomson0119 <kjs980119@gmail.com>
2022-04-21 23:18:48 +0900

    onPause, onResume, onDestroy 시 적절한 행동 수행

--- TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Main/MainActivity.java
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Main/MainActivity.java
@@ -1,13 +1,35 @@
-package kr.ac.tukorea.gamekim2016180014.touchandslice;
+package kr.ac.tukorea.gamekim2016180014.touchandslice.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import kr.ac.tukorea.gamekim2016180014.touchandslice.GameScene.GameScene;
+import kr.ac.tukorea.gamekim2016180014.touchandslice.GameScene.ScreenView;
+import kr.ac.tukorea.gamekim2016180014.touchandslice.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        ScreenView.view.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ScreenView.view.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        ScreenView.view = null;
+        super.onDestroy();
     }
 }
