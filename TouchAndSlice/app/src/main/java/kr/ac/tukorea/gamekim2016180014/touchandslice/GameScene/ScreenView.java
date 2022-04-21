b8186bf96d686cbae643bba65ccf6ea644471684
Tomson0119 <kjs980119@gmail.com>
2022-04-21 23:18:24 +0900

    Time을 보여주는 text view 추가

--- TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/GameScene/ScreenView.java
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/GameScene/ScreenView.java
@@ -1,4 +1,4 @@
-package kr.ac.tukorea.gamekim2016180014.touchandslice;
+package kr.ac.tukorea.gamekim2016180014.touchandslice.GameScene;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -9,23 +9,33 @@ import android.util.Log;
 import android.view.Choreographer;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-import java.util.ArrayList;
+import kr.ac.tukorea.gamekim2016180014.touchandslice.Common.Metrics;
+import kr.ac.tukorea.gamekim2016180014.touchandslice.R;
 
 public class ScreenView extends View implements Choreographer.FrameCallback {
     public static ScreenView view;
+    public static TextView timeTextView;
+    private static boolean loop;
 
+    private float totalSec;
     private float elapsedSec;
     private long prevNs;
     private boolean initialized;
-    private Paint fpsPaint;
+    private final Paint fpsPaint;
 
     public ScreenView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
         view = this;
         view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        loop = true;
+
         fpsPaint = new Paint();
         fpsPaint.setColor(Color.RED);
         fpsPaint.setTextSize(100);
@@ -36,8 +46,10 @@ public class ScreenView extends View implements Choreographer.FrameCallback {
         super.onSizeChanged(w, h, old_w, old_h);
         Metrics.setResolution(w, h);
 
-        if(initialized == false) {
+        if(!initialized) {
             initialized = true;
+            FrameLayout parent = (FrameLayout)getParent();
+            timeTextView = (TextView)parent.findViewById(R.id.timeTextId);
             GameScene.getInstance().init();
             Choreographer.getInstance().postFrameCallback(this);
         }
@@ -45,10 +57,12 @@ public class ScreenView extends View implements Choreographer.FrameCallback {
 
     @Override
     public void doFrame(long currNs) {
-        calculateTimeStep(currNs);
-        update();
-        invalidate();
-        Choreographer.getInstance().postFrameCallback(this);
+        if(loop) {
+            calculateTimeStep(currNs);
+            update();
+            invalidate();
+            Choreographer.getInstance().postFrameCallback(this);
+        }
     }
 
     @Override
@@ -58,7 +72,24 @@ public class ScreenView extends View implements Choreographer.FrameCallback {
 
     private void calculateTimeStep(long currNs) {
         elapsedSec = (float)(currNs - prevNs) / 1_000_000_000;
+        if(prevNs == 0) elapsedSec = 0.0f;
         prevNs = currNs;
+
+        totalSec += elapsedSec;
+        updateTimeText();
+    }
+
+    private void updateTimeText() {
+        int minute = (int)(totalSec / 60);
+        int second = (int)(totalSec - minute * 60);
+
+        String minStr = String.valueOf(minute);
+        if(minute < 10) minStr = "0" + minStr;
+
+        String secStr = String.valueOf(second);
+        if(second < 10) secStr = "0" + secStr;
+
+        timeTextView.setText(minStr + ":" + secStr);
     }
 
     private void update() {
@@ -69,5 +100,18 @@ public class ScreenView extends View implements Choreographer.FrameCallback {
     protected void onDraw(Canvas canvas) {
         GameScene.getInstance().draw(canvas);
         canvas.drawText(String.valueOf((int)(1/elapsedSec)), 10, 100, fpsPaint);
+    }
+
+    public void onPause() {
+        loop = false;
+        System.out.println("Paused!");
+    }
+
+    public void onResume() {
+        if(loop == false) {
+            loop = true;
+            Choreographer.getInstance().postFrameCallback(this);
+            System.out.println("Resumed!");
+        }
     }
 }
