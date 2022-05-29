b4ec037e88c955ae11662e23db791618067353d2
Tomson0119 <kjs980119@gmail.com>
2022-05-29 12:46:14 +0900

    Result Activity 추가, view 설정

---
+++ TouchAndSlice/app/src/main/java/kr/ac/tukorea/gamekim2016180014/touchandslice/Main/ResultActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.tukorea.gamekim2016180014.touchandslice.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.ac.tukorea.gamekim2016180014.touchandslice.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
